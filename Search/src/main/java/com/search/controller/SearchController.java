10c4184124e6f33e8ebbc430c73c8465da60339c
priyanka109905 <priyankaborase22@gmail.com>
2022-01-05 19:51:38 +0530

    Search

---
+++ Search/src/main/java/com/search/controller/SearchController.java
@@ -0,0 +1,58 @@
+package com.search.controller;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.search.entity.FlightDetails;
+import com.search.service.SearchService;
+
+import io.swagger.annotations.ApiOperation;
+
+@CrossOrigin("*")
+@Component
+@RestController
+@RequestMapping("/search")
+public class SearchController 
+{
+	@Autowired
+	private SearchService adminService;
+	
+	@GetMapping("/allflights")
+	@ApiOperation(value="Get all flight details")
+	public List<FlightDetails> getAllDetails()
+	{
+		return adminService.getAllDetails();
+	}
+	
+	@GetMapping("/status/")
+	@ApiOperation(value="Get status of your booking by Pnr Number")
+	public String getStatus(@RequestParam long pnrNo)
+	{
+		return adminService.pnrStatus(pnrNo);
+	}
+	
+	@GetMapping("/flightNo/")
+	@ApiOperation(value="Get flight details by flight Number")
+	public FlightDetails getDetailsByflightNo(@RequestParam int flightNo) 
+	{
+		return adminService.getDetailsByFlightNo(flightNo);
+	}
+	
+	@GetMapping("/findBy/{startPoint}/{endPoint}/{flightDate}")
+	@ApiOperation(value="Get Flight details by giving startPoint and endPoint locations and date")
+	public FlightDetails[] getFlightDetailsByStartPointAndEndPointAndFlightDate(@PathVariable String startPoint,@PathVariable  String endPoint,@PathVariable String flightDate)
+	{
+		RestTemplate restTemplate=new RestTemplate();
+		FlightDetails[] response=restTemplate.getForObject("http://localhost:8081/fare/findBy/"+startPoint +"/"+endPoint+"/"+flightDate,FlightDetails[].class);
+		return response;
+		
+	}
+}
---
+++ Search/src/main/java/com/search/service/SearchServiceImpl.java
@@ -0,0 +1,59 @@
+package com.search.service;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.search.entity.FlightDetails;
+import com.search.entity.PaymentDetails;
+import com.search.repository.SearchRepository;
+import com.search.repository.UserPaymentRepository;
+
+
+@Service
+public class SearchServiceImpl implements SearchService
+{
+	@Autowired
+	private SearchRepository adminRepo;
+
+	@Autowired
+	private UserPaymentRepository userPayRepo;
+
+	@Override
+	public List<FlightDetails> getAllDetails() {
+		List<FlightDetails> FlightDetails = new ArrayList<FlightDetails>();
+		adminRepo.findAll().forEach(FlightDetails1 -> FlightDetails.add(FlightDetails1));
+		return FlightDetails;
+	}
+
+	@Override
+	public String pnrStatus(long pnrNo) {
+		Random rand = new Random();
+		List<String> status = new ArrayList<String>();
+		status.add("Confirm");
+		status.add("Waiting list");
+		List<PaymentDetails> paymentList = userPayRepo.findAll();
+		for (PaymentDetails det : paymentList) {
+			if (det.getPnrNo() == pnrNo) {
+				return status.get(rand.nextInt(status.size()));
+			}
+		}
+		return "Ticket is not booked with PNR Number " + pnrNo;
+	}
+
+	@Override
+	public FlightDetails getDetailsByFlightNo(int flightNo) {
+		return adminRepo.findByFlightNo(flightNo);
+
+	}
+
+	@Override
+	public List<FlightDetails> findByStartPointAndEndPointAndFlightDate(String startPoint, String endPoint,
+			String flightDate) {
+		return adminRepo.findByStartPointAndEndPointAndFlightDate(startPoint, endPoint, flightDate);
+
+	}
+
+}
